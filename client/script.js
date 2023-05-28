5831f61d6d08586a85245610cd14368ca635fceb
Adam Young <adamyoungy678@gmail.com>
2023-05-28 23:15:27 +0100

    feat: created a loader function for the dots while waiting for response

--- client/script.js
+++ client/script.js
@@ -5,3 +5,15 @@ const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
+
+function loader(element) {
+    element.textContent = '';
+
+    loadInterval = setInterval(() => {
+        element.textContent += '.';
+
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300)
+}
