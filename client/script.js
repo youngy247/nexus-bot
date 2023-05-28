1aebdd3965f2f5f446503e776111176ea088a418
Adam Young <adamyoungy678@gmail.com>
2023-05-28 23:36:49 +0100

    feat: added scrolling to the bottom feature when submitting a chat

--- client/script.js
+++ client/script.js
@@ -71,7 +71,11 @@ const handleSubmit = async (e) => {
     const uniqueId = generateUniqueId();
     chatContainer.innerHTML += chatStripe(true, "", uniqueId);
 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
     const messageDiv = document.getElementById(uniqueId);
 
     loader(messageDiv);
 }
+
+f
