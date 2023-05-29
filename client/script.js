0263864287a3c22619b71a476448938062386c66
Adam Young <adamyoungy678@gmail.com>
2023-05-29 13:19:28 +0100

    complete: Handled the response from the server and displayed the bot's message or an error message based on the response status.

--- client/script.js
+++ client/script.js
@@ -91,6 +91,19 @@ const handleSubmit = async (e) => {
 
     clearInterval(loadInterval);
     messageDiv.innerHTML = '';
+
+    if(response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+
+        typeText(messageDiv, parsedData);
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong";
+
+        alert(err);
+    } 
 }
 
 form.addEventListener('submit', handleSubmit);
