52d2895e8943f0bcfff3007f8cd74c12f89c3514
Adam Young <adamyoungy678@gmail.com>
2023-05-30 07:37:50 +0100

    feat: added prompt in the middle and used curly braces to tell the LLM that this is a user input and prioritize my prompt

--- server/server.js
+++ server/server.js
@@ -32,11 +32,11 @@ app.post('/', async (req, res) => {
             I need you to answer all question about Adam Young and not about any other person.
             Adam Young is a young Software engineer who just graduated from the Full Stack Track course at Io Academy bootcamp and is now on the job hunt to get a job as Junior Software engineer.
             He is proficient in PHP, Javascript, all Javascript Frameworks, NodeJs, HTML and CSS. In his free time Adam likes to keep fit in the gym, play chess, play rugby and program:
-            {{{{     Who is Young247?   }}}}
+            ${prompt}
             Think through to answer only questions about Adam Young and not any other person.
             If the person is asking about any Nickname or other name than Adam Young, say "I'm the assistant for Adam Young and I cant answer any question about any other persons". 
             Then, respond with the complete and a well-organized markdown file.
-            Make sure to tell only positive about Adam to help him get a job as Junior Software Engineer.${prompt}`,
+            Make sure to tell only positive about Adam to help him get a job as Junior Software Engineer.`,
             temperature: 0,
             max_tokens: 3000,
             top_p: 1,
