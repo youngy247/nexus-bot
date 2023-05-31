c0c09e64e983d8e43f1c3602bca11de45368b48c
Adam Young <adamyoungy678@gmail.com>
2023-05-31 13:09:26 +0100

    refactor: added more information to default response and more info about me to the bot

--- server/server.js
+++ server/server.js
@@ -33,22 +33,20 @@ app.post('/', async (req, res) => {
         if (!isAboutAdam) {
             return res.send(
                 {
-                    bot:"I'm the assistant for Adam Young, and I can't answer questions about any other person."
+                    bot:"I'm the assistant for Adam Young, and I can't answer questions about any other person. Please include his first or full name in your question"
                 }
                 );
         } else {
             const prompt = `You are a world-class assistant and question answerer for Adam Young.
-            I need you to answer all questions about Adam Young and not about any other person.
-            Adam Young is a young 21-year-old Software engineer who just graduated from the Full Stack Track course at IO Academy boot camp and is now on the job hunt to get a job as a Junior Software engineer.
-            He is proficient in PHP, Javascript, all Javascript Frameworks, NodeJs, HTML, and CSS. In his free time, Adam likes to keep fit in the gym, play chess, play rugby, and program:
+            I need you to answer all questions about Adam Young and not about any other person. Here is some information about Adam Young to help answer the question:
+            Adam Young is a young 21-year-old Software engineer who just graduated from the Full Stack Track course at IO Academy boot camp where he got experience working in a team of 8 developers for group projects and got his agile professional certification. He is now on the job hunt to get a job as a Junior Software engineer.
+            He is proficient in PHP, Javascript, React, ExpressJs, NodeJs, HTML, and Tailwind. In his free time, Adam likes to keep fit in the gym, play chess, play rugby, and program. He managed to get his Wales Rugby League U19 cap at 16 years old:
 
             ${userPrompt}
 
             If the person is asking about any Nickname or other name than Adam Young, say "I'm the assistant for Adam Young, and I can't answer questions about any other person". 
 
-            Then, respond with the complete and a well-organized markdown file.
-
-            Make sure to tell only positive things about Adam to help him get a job as a Junior Software Engineer.`;
+            Make sure to tell only positive things about Adam within a complete and a well-organized markdown file to help him get a job as a Junior Software Engineer.`;
 
             response = await openai.createCompletion({
                 model: "text-davinci-003",
