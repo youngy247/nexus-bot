6e9d79134980369dd9a5b0b58b04500c6a34776d
Adam Young <adamyoungy678@gmail.com>
2023-06-01 08:09:10 +0100

    feat: added bot greeting if the user input is empty

--- server/server.js
+++ server/server.js
@@ -30,17 +30,20 @@ app.post('/', async (req, res) => {
         const isAboutAdam = userPrompt.toLowerCase().includes("adam") || userPrompt.toLowerCase().includes("adam young");
         
         let response;
-        if (!isAboutAdam) {
-            return res.send(
-                {
-                    bot:"I'm the assistant for Adam Young, and I can't answer questions about any other person. Please include his first or full name in your question"
-                }
-                );
+        if (userPrompt === "") {
+            // Greet the user when there is no user prompt
+            return res.send({
+                bot: "Welcome to Adam Young's Personal Assistant! I'm here to provide you with information and answer any questions you have about Adam's skills, projects, and experiences as a recent graduate in software engineering."
+            });
+        } else if (!isAboutAdam) {
+            return res.send({
+                bot: "I'm the assistant for Adam Young, and I can't answer questions about any other person. Please include his first or full name in your question"
+            });
         } else {
             const prompt = `You are a world-class assistant and question answerer for Adam Young.
             I need you to answer all questions about Adam Young and not about any other person. Here is some information about Adam Young to help answer the question:
             Adam Young is a young 21-year-old Software engineer who just graduated from the Full Stack Track course at IO Academy boot camp where he got experience working in a team of 8 developers for group projects and got his agile professional certification. He is now on the job hunt to get a job as a Junior Software engineer.
-            He is proficient in PHP, Javascript, React, ExpressJs, NodeJs, HTML, and Tailwind. In his free time, Adam likes to keep fit in the gym, play chess, play rugby, and program. He managed to get his Wales Rugby League U19 cap at 16 years old:
+            He is proficient in PHP, Javascript, React, ExpressJs, NodeJs, HTML, and Tailwind. In his free time, Adam likes to keep fit in the gym, play chess, play rugby, and program. He managed to get his Wales Rugby League U19 cap at 16 years old.:
 
             ${userPrompt}
 
