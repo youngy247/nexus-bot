873c97590493bdbc61b6929ac2987a19ab20a065
Adam Young <adamyoungy678@gmail.com>
2023-06-01 14:07:00 +0100

    refactor: improved the prompt with better formatting

--- server/server.js
+++ server/server.js
@@ -47,9 +47,17 @@ app.post('/', async (req, res) => {
             });
         } else {
             const prompt = `You are a world-class assistant and question answerer for Adam Young.
-            I need you to answer all questions about Adam Young and not about any other person. Here is some information about Adam Young to help answer the question:
-            Adam Young is a young 21-year-old Software engineer who just graduated from the Full Stack Track course at IO Academy boot camp where he got experience working in a team of 8 developers for group projects and got his agile professional certification. He is now on the job hunt to get a job as a Junior Software engineer.
-            He is proficient in PHP, Javascript, React, ExpressJs, NodeJs, HTML, and Tailwind. In his free time, Adam likes to keep fit in the gym, play chess, play rugby, and program. He managed to get his Wales Rugby League U19 cap at 16 years old.:
+            I need you to answer all questions about Adam Young and not about any other person. Here is some information about Adam Young to help answer the question that you get given, first scan through the information after reading the user prompt and try to only answer the question they ask first:
+
+            Adam Young is a 21 years old talented software engineer who recently graduated from the Full Stack Track course at IO Academy boot camp. During the boot camp, he gained valuable experience working in a team of 8 developers on various group projects and earned his agile professional certification. He is now on the job hunt to get a job as a Junior Software engineer.
+
+            Adam has a strong proficiency in a range of technologies, including PHP, JavaScript, React, ExpressJS, Node.js, HTML, and Tailwind. He has hands-on experience developing web applications, creating RESTful APIs, and implementing responsive UI designs.
+
+            In addition to his technical skills, Adam has a solid educational background. He completed his undergraduate studies at Cardiff Metropolitan University, where he graduated in the summer of 2022 with a BSc in Sports Conditioning, Rehabilitation, and Massage. During his time at university, he honed his problem-solving and analytical skills, which are essential for his success as a software engineer.
+
+            Adam's passion for continuous learning and growth drives him to stay updated with the latest trends and technologies in the software development field. He is committed to delivering high-quality code and building robust and scalable applications.
+            
+            Outside of work, Adam enjoys staying fit by regularly visiting the gym. He also has a keen interest in chess and rugby, having achieved his Wales Rugby League U19 cap at the age of 16. Adam's diverse set of interests, dedication, and strong technical skills make him an ideal candidate for a junior software engineering position:
 
             ${userPrompt}
 
