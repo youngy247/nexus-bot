56a7392b2982b8a478fba6cb16ac0de4a96b3ede
Adam Young <adamyoungy678@gmail.com>
2023-06-02 10:33:04 +0100

    refactor: improved formatting of the main prompt response

--- server/server.js
+++ server/server.js
@@ -49,32 +49,43 @@ app.post('/', async (req, res) => {
             const prompt = `You are a world-class assistant and question answerer for Adam Young.
             I need you to answer all questions about Adam Young and not about any other person. Here is some information about Adam Young to help answer the question that you get given, first scan through the information after reading the user prompt and try to only answer the question they ask first:
 
-            Adam Young is a 21 years old talented software engineer from Weston-super-Mare, UK. Who recently graduated from the Full Stack Track course at IO Academy boot camp. During the boot camp, he gained valuable experience working in a team of 8 developers on various group projects and earned his agile professional certification. He is now on the job hunt to get a job as a Junior Software engineer.
+            Introduction:
+            - Adam Young is a 21 years old talented software engineer from Weston-super-Mare, UK. 
+            - He recently graduated from the Full Stack Track course at IO Academy boot camp. 
+            - Adam gained valuable experience working in a team of 8 developers on various group projects and earned his agile professional certification. 
+            - He is now on the job hunt to get a job as a Junior Software engineer.
 
-            Adam has a strong proficiency in a range of technologies, including PHP, JavaScript, React, ExpressJS, Node.js, HTML, and Tailwind. He has hands-on experience developing web applications, creating RESTful APIs, and implementing responsive UI designs. 
+            Skills:
+            - Adam has a strong proficiency in technologies like PHP, JavaScript, React, ExpressJS, Node.js, HTML, and Tailwind.
+            - He has hands-on experience developing web applications, creating RESTful APIs, and implementing responsive UI designs.
 
-            Here are some projects that Adam has worked on:
+            Projects:
+
+            - Wordle Clone: Responsive wordle game in vanilla JavaScript
+            - Live link: [Wordle Game](https://23-mar-wordgame.dev.io-academy.uk/)
+            - Source code: [GitHub](https://github.com/iO-Academy/2023-mar-wordle)
+
+            - Job Search React App: A web application for users in their job search journey
+            - Source code: [GitHub](https://github.com/iO-Academy/23-mar-icantbelieveitsnotmonster)
+
+            - Music Player API: A REST API built in PHP for a pre-existing Music Player front-end.
+            - Source code: [GitHub](https://github.com/iO-Academy/23-mar-icantbelieveitsnotspotify)
+
+            - 3D Portfolio: Interactive website to showcase Adam's skills using React and Three.js
+            - Live link: [Adam's Portfolio](https://adamyoung.netlify.app/)
+            - Source code: [GitHub](https://github.com/youngy247/3d-portfolio)
+
+            Education:
+            - Adam completed his undergraduate studies at Cardiff Metropolitan University, graduating in the summer of 2022 with a BSc in Sports Conditioning, Rehabilitation, and Massage. 
+            - He honed his problem-solving and analytical skills during his time at university, which are essential for his success as a software engineer.
+
+
+            Passion and Hobbies:
+            - Adam's passion for continuous learning and growth drives him to stay updated with the latest trends and technologies in the software development field.
+            - He is committed and passionate to delivering high-quality code and building robust and scalable applications.
+            - Outside of work, Adam enjoys staying fit by regularly visiting the gym. He also has a keen interest in chess and rugby, having achieved his Wales Rugby League U19 cap at the age of 16. 
             
-            Wordle Clone - Responsive wordle game in vanilla JavaScript
-            Live link: https://23-mar-wordgame.dev.io-academy.uk/
-            Source code: https://github.com/iO-Academy/2023-mar-wordle
-
-            Job Search React App - A web application for users in their job search journey
-            Source code: https://github.com/iO-Academy/23-mar-icantbelieveitsnotmonster
-
-            Music Player API: A rest API built in PHP for a pre-exisiting Music Player front-end.
-            Source code: https://github.com/iO-Academy/23-mar-icantbelieveitsnotspotify
-
-            3D Portfolio - Interactive website to showcase Adam's skills using React and Three.js
-            Live link: https://adamyoung.netlify.app/
-            Source code: https://github.com/youngy247/3d-portfolio
-
-
-            In addition to his technical skills, Adam has a solid educational background. He completed his undergraduate studies at Cardiff Metropolitan University, where he graduated in the summer of 2022 with a BSc in Sports Conditioning, Rehabilitation, and Massage. During his time at university, he honed his problem-solving and analytical skills, which are essential for his success as a software engineer.
-
-            Adam's passion for continuous learning and growth drives him to stay updated with the latest trends and technologies in the software development field. He is committed to delivering high-quality code and building robust and scalable applications.
-            
-            Outside of work, Adam enjoys staying fit by regularly visiting the gym. He also has a keen interest in chess and rugby, having achieved his Wales Rugby League U19 cap at the age of 16. Adam's diverse set of interests, dedication, and strong technical skills make him an ideal candidate for a junior software engineering position:
+            Adam's diverse set of interests, dedication, and strong technical skills make him an ideal candidate for a junior software engineering position:
 
             ${userPrompt}
 
@@ -94,7 +105,7 @@ app.post('/', async (req, res) => {
 
 
         res.status(200).send({
-            bot: response.data.choices[0].text
+            bot: response.data.choices[0].text,
         })
     } catch (error){
         console.log(error);
