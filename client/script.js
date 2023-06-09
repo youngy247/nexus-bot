ac46217568adc2925c17514b2aa1cf8a5cd7543c
Adam Young <adamyoungy678@gmail.com>
2023-06-09 15:10:19 +0100

    fix: changed fetch requests back to remote server host url

--- client/script.js
+++ client/script.js
@@ -116,7 +116,7 @@ const handleSubmit = async (e) => {
 
     // fetch data from server -> bot's response
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://nexus-bnue.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -149,7 +149,7 @@ const handleSubmit = async (e) => {
 
 async function fetchInitialGreeting() {
     try {
-      const response = await fetch('http://localhost:5000');
+      const response = await fetch('https://nexus-bnue.onrender.com');
       if (response.ok) {
         const data = await response.json();
         const initialGreeting = data.bot.trim();
