23f3170d5ee77723601e44034e21074c1de0d973
Adam Young <adamyoungy678@gmail.com>
2023-06-14 12:39:09 +0100

    feat: added class attrubite in custom policy

--- client/script.js
+++ client/script.js
@@ -138,7 +138,7 @@ const handleSubmit = async (e) => {
 
     const customPolicy = {
       ALLOWED_TAGS: ['a', 'p', 'h2', 'ul', 'li', 'img',],
-      ALLOWED_ATTR: ['href', 'target', 'src', 'alt'],
+      ALLOWED_ATTR: ['href', 'target', 'src', 'alt', 'class'],
       ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel):|[^a-z]|[a-z+.-]+(?:[^a-z+.-]|$))/i,
       ADD_ATTR: [['target', '_blank']],
     };
