c0e88ceb433ca04c0787a485574b28a075afd746
Adam Young <adamyoungy678@gmail.com>
2023-07-05 16:37:32 +0100

    refactor: reduced rate limit to 10/minute

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
  // Create a rate limiter
  const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 20, // 20 requests per minute
+  max: 10, // 10 requests per minute
   keyGenerator: (req) => req.ip,
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
