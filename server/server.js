91ddecb08557d5f1cd21d6f1e8aef3a1b07f2871
Adam Young <adamyoungy678@gmail.com>
2023-07-05 17:13:21 +0100

    fix: correctly getting users ip for rate limiting

--- server/server.js
+++ server/server.js
@@ -20,11 +20,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
  // Create a rate limiter
+
+ const keyGenerator = (req) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  return ip;
+};
+
  const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
   max: 10, // 10 requests per minute
-  keyGenerator: (req) => req.ip,
+  keyGenerator,
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: 'Please do not spam me'
